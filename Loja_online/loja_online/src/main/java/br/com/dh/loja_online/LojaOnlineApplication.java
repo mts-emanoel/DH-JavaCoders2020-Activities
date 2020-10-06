fedb2e30272cecbc49fc089a858cc818b4920265
Matheus Emanoel Rodrigues De Albuquerque <lab.emanoel@gmail.com>
2020-10-06 00:18:31 -0300

    Start project Loja_Online

---
+++ Loja_online/loja_online/src/main/java/br/com/dh/loja_online/LojaOnlineApplication.java
@@ -0,0 +1,13 @@
+package br.com.dh.loja_online;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaOnlineApplication.class, args);
+	}
+
+}
---
+++ Loja_online/loja_online/src/test/java/br/com/dh/loja_online/LojaOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dh.loja_online;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
